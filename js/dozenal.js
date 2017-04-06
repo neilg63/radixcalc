6bc6ccf4b72d0cfb8c9a99df5af69915e43dd0eb
Neil Gardner <neilgardner1963@gmail.com>
2017-04-07 00:16:24 +0100

    added icon set

--- js/dozenal.js
+++ js/dozenal.js
@@ -1,3 +1,57 @@
+function RationalFraction(decFrac,divisor) {
+  if (typeof divisor == 'number') {
+    var n=2,mult;
+    if (decFrac < 1 && decFrac > 0) {
+      mult = 1/decFrac;  
+      divisor *= mult;
+      decFrac *= mult;
+      var rem = (divisor * mult) % 1;
+      if (rem !== 0) {
+        var fr = new RationalFraction(rem);
+        divisor *= fr.divisor;
+        decFrac *= fr.divisor;
+      } else {
+        divisor = Math.round(divisor);
+      }
+    }
+    this.multiple = decFrac;
+    this.divisor = divisor;
+    if (divisor > 3) {
+      for (; n <= (divisor/2);n++) {
+        if (divisor % n === 0) {
+          mult = this.multiple / (this.divisor/n);
+          if (mult >= 1 && console % 1 === 0) {
+            this.multiple = Math.round(mult);
+            this.divisor = Math.round(n);
+            break;
+          }
+        }
+      }
+    }
+    this.dec = decFrac / divisor;
+  } else {
+    this.dec = decFrac;
+    var convert = function(decFrac) {
+      if (typeof decFrac == 'number' && decFrac > 0) {
+        var n=2, units=0;
+        for (;n<1024;n++) {
+          units = decFrac * n;
+          if (units % 1 < 0.000001 && units % 1 > -0.000001) {
+            return [Math.round(units),n];
+          }
+        }
+      }
+      return [0,0];
+    }
+    var frac = convert(decFrac);
+    this.multiple = frac[0];
+    this.divisor = frac[1];
+  }
+}
+
+
+
+
 var app = new Vue({
   el: '#radix-calc',
   data: {
@@ -6,24 +60,48 @@ var app = new Vue({
     maxResults: 50,
     system: 'place-value',
     showDec: true,
-    result: 0,
-    resultOut:[],
+    result: {
+      base: 0,
+      out: [],
+      decVal:0,
+      decFrac: 0,
+      ratFrac: RationalFraction(0),
+      baseFrac: {multiple:0,divisor:0},
+    },
+    prev: {
+      base: 0,
+      decVal:0,
+      out: [],
+      decFrac: 0,
+      ratFrac: RationalFraction(0),
+      baseFrac: {multiple:0,divisor:0},
+    },
     radixClass: 'radix-12',
     placeSep: '.',
-    decResult:0,
     resultSets: [],
     hasResultSets: false,
-    prevVal: 0,
     showPrev: false,
-    prevNum: 0,
+    showRational: false,
     baseNums: [],
-    prevOut: [],
     layoutMode: 'base-12 rows-4-across',
     mode: '',
+    prevMode: '',
     prevClicked: '',
     resultPadClass:'',
     showOptions: false,
     optionsHideable:true,
+    baseName: "Dozenal",
+    bases: [
+      {id:"2", num: 2, name: "Binary"},
+      {id:"8", num: 8, name: "Octal"},
+      {id:"10", num: 10, name: "Decimal"},
+      {id:"10-roman", num: 10, name: "Roman"},
+      {id:"12", num: 12, name: "Dozenal"},
+      {id:"16", num: 16, name: "Hexadeximal"},
+      {id:"20", num: 20, name: "Vigesimal"},
+      {id:"36", num: 36, name: "Base 36"},
+      {id:"60", num: 60, name: "Sexagesimal"},
+    ],
     romanBaseNums: [
       {value: 1,name: 'I',id: 'num-i','class': 'cell-num-i'},
       {value: 5,name: 'V',id: 'num-v','class': 'cell-num-v'},
@@ -70,17 +148,19 @@ var app = new Vue({
         if (d.hasOwnProperty('showDec')) {
           this.showDec = d.showDec;
         }
+        if (d.hasOwnProperty('showRational')) {
+          this.showRational = d.showRational;
+        }
       }
     }
     this.updateBase();
-
   },
   watch: {
-    result: function() {
-      this.resultOut = this.renderOut(this.result);
+    'result.base': function() {
+      this.updateNum(this.result);
     },
-    prevNum: function() {
-      this.prevOut = this.renderOut(this.prevNum);
+    'prev.base': function() {
+      this.updateNum(this.prev);
     },
     base: function() {
       this.updateOptions();
@@ -90,7 +170,10 @@ var app = new Vue({
     },
     showDec: function() {
       this.updateOptions();
-    }
+    },
+    showRational: function() {
+      this.updateOptions();
+    },
   },
   methods: {
     loadNums: function() {
@@ -149,7 +232,7 @@ var app = new Vue({
       this.baseNums=this.romanBaseNums;
     },
     enterNum: _.debounce(function(num,name) {
-      var res = this.result.toString();
+      var res = this.result.base.toString();
       if (res == '0' || res == 'a0') {
         res = '';
       }
@@ -157,20 +240,20 @@ var app = new Vue({
         name = 'a0';
       }
       if (this.validateNum(num,name)) {
-        this.result = res + name;
+        this.result.base = res + name;
         if (this.system == 'roman' && this.base == 10) {
           this.rewriteRomanResult(num);
         }
       }
       
-      this.decResult = this.convert(this.result);
+      this.result.decVal = this.convert(this.result.base);
       this.prevClicked = name;
     },25),
     validateNum: function(num,name) {
       return true;
     },
     rewriteRomanResult: function(num) {
-      var chars = this.result.split(''),
+      var chars = this.result.base.split(''),
         len=chars.length,i=(len-1),
         low = len>5? len-5 : 0,index=-2,
         prevIndex=-2,penIndex=-2,prevCount=0,next=-2;
@@ -181,21 +264,23 @@ var app = new Vue({
           if (index == prevIndex) {
             prevCount++;
             if (isFive) {
-              this.result = this.result.substring(0,(len-2)) + this.romanSymbols[(index+1)];
+              this.result.base = this.result.base.substring(0,(len-2)) + this.romanSymbols[(index+1)];
             } else if (prevCount>2) {
               next = index + 1;
               if (next < this.romanSymbols.length) {
-                this.result = this.result.substring(0,(len-3)) + this.romanSymbols[next];
+                this.result.base = this.result.base.substring(0,(len-3)) + this.romanSymbols[next];
               }
             }
           } else {
             prevCount=0;
           }
           if (index == penIndex && ((index+1) == prevIndex || (index+2) == prevIndex)) {
-            this.result = this.renderRoman(this.decResult + num);
+            this.result.base = this.renderRoman(this.result.decVal + num);
           }
           else if (penIndex >= 0 && prevIndex <= penIndex && index >= prevIndex) {
-            this.result = this.renderRoman(this.decResult + num);
+            if (isFive || prevCount > 2) {
+              this.result.base = this.renderRoman(this.result.decVal + num);
+            }
           }
           penIndex = prevIndex;
           prevIndex = index;
@@ -203,8 +288,8 @@ var app = new Vue({
       }
     },
     enterDot: function() {
-      if (this.result.contains(this.placeSep)==false) {
-        this.result += this.placeSep;
+      if (this.result.base.contains(this.placeSep)==false) {
+        this.result.base += this.placeSep;
         this.prevClicked = this.placeSep;
       }
     },
@@ -262,8 +347,11 @@ var app = new Vue({
         case 60:
           this.layoutMode = 'base-60 rows-10-across';
           break;
+      }var bId = this.baseId;
+      var base = _.find(this.bases,function(b) {return b.id == bId});
+      if (base) {
+        this.baseName = base.name;
       }
-      //this.placeSep = this.base == 10? '.' : ';';
       this.clear();
     },
     _operate: function(newMode) {
@@ -274,8 +362,8 @@ var app = new Vue({
       if (prevMode != '' && prevMode != '=') {
         this.calc(newMode);
       }
-      this.prevVal = this.decResult;
-      this.prevNum = this.result;
+      this.prev.decVal = this.result.decVal;
+      this.prev.base = this.result.base;
       this.zeroResult();
     },
     add:function() {
@@ -293,66 +381,66 @@ var app = new Vue({
     power:function() {
       this._operate('^');
     },
+    toBase: function(decRes) {
+      if (this.base <= 36) {
+        var res = decRes.toString(this.base);
+      } else {
+        var res = this.renderLarge(decRes);
+      }
+      switch (this.base) {
+        case 12:
+          res = res.replace(/a/g,'d').replace(/b/g,'e');
+          break;
+        case 10:
+          if (this.system == 'roman') {
+            res = this.renderRoman(decRes);
+          }
+          break;
+      }
+      return res;
+    },
     calc:function(op) {
       if (!op) {
         op = '=';
       }
       var resultSet = {
-        first: this.prevNum,
+        first: this.prev.base,
         operator: this.mode,
-        second: this.result,
+        second: this.result.base,
         base: this.base,
         result: 0,
+        rational: null,
         dec: {
-          first: this.prevVal,
-          second: this.decResult,
+          first: this.prev.decVal,
+          second: this.result.decVal,
           result: 0
         },
       };
       if (this.mode !== '') {
-        var num1 = parseFloat(this.prevVal),
-          num2 = parseFloat(this.decResult);
+        var num1 = parseFloat(this.prev.decVal),
+          num2 = parseFloat(this.result.decVal);
         switch(this.mode) {
           case '+':
-            this.decResult = num1 + num2;
+            this.result.decVal = num1 + num2;
             break;
           case '-':
-            this.decResult = num1 - num2;
+            this.result.decVal = num1 - num2;
             break;
           case '*':
-            this.decResult = num1 * num2;
+            this.result.decVal = num1 * num2;
             break;
           case '^':
-            this.decResult = Math.pow(num1,num2);
+            this.result.decVal = Math.pow(num1,num2);
             break;
           case '/':
             if (num2 !== 0) {
-              this.decResult = num1 / num2;
+              this.result.decVal = num1 / num2;
             }
             break;
         }
-        if (this.base <= 36) {
-          var res = this.decResult.toString(this.base);
-        } else {
-          var res = this.renderLarge(this.decResult);
-        }
-        switch (this.base) {
-          case 12:
-            this.result = res.replace(/a/g,'d').replace(/b/g,'e');
-            break;
-          case 10:
-            if (this.system == 'roman') {
-              this.result = this.renderRoman(this.decResult);
-            } else {
-              this.result = res;
-            }
-            break;
-          default:
-            this.result = res;
-            break;
-        }
-        resultSet.result = this.result;
-        resultSet.dec.result = this.decResult;
+        this.result.base = this.toBase(this.result.decVal);
+        resultSet.result = this.result.base;
+        resultSet.dec.result = this.result.decVal;
         this.resultSets.unshift(resultSet);
         if (this.resultSets.length > this.maxResults) {
           this.resultSets.pop();
@@ -361,6 +449,7 @@ var app = new Vue({
         if (op == '=') {
           this.hasResultSets = true;
           this.showPrev = false;
+          this.prevMode = this.mode;
           this.mode = '';
           this.prevClicked = '=';
         }
@@ -370,9 +459,9 @@ var app = new Vue({
       return resultSet;
     },
     clear:function() {
-      if (this.showPrev && this.decResult === 0) {
-        this.prevNum = '0';
-        this.prevVal = 0;
+      if (this.showPrev && this.result.decVal === 0) {
+        this.prev.base = '0';
+        this.prev.decVal = 0;
         this.showPrev = false;
       } else {
         this.zeroResult();
@@ -416,6 +505,21 @@ var app = new Vue({
       } else {
         return this._convertDecCols(numStr);
       }
+    },
+    updateNum: function(num) {
+      num.out = this.renderOut(num.base);
+      num.decFrac = num.decVal % 1;
+      
+      if (this.prevMode == '/' && this.resultSets.length>0) {
+        var r = this.resultSets[0];
+        if (r.operator == '/') {
+          num.ratFrac = new RationalFraction(r.dec.first,r.dec.second);
+        }
+      } else {
+        num.ratFrac = new RationalFraction(num.decFrac);
+      }
+      num.baseFrac = this.convertBaseFrac(num.ratFrac);
+      return num;
     },
     _convertRoman: function(numStr) {
       var dec=0;
@@ -582,7 +686,7 @@ var app = new Vue({
                   pv = this.romanNums[pk];
                 }
                 out += pk + k;
-                rem -= (pv + v);
+                rem -= (v - pv);
               }
               
             }
@@ -594,20 +698,20 @@ var app = new Vue({
     zeroResult: function() {
       switch (this.system) {
         case 'roman':
-          this.result ='';
+          this.result.base ='';
           break;
         default:
-          this.result ='0';
+          this.result.base ='0';
           break;
       }
-      this.decResult = 0;
+      this.result.decVal = 0;
     },
     backspace: function() {
-      var len = this.result.length;
+      var len = this.result.base.length;
       if (len > 1) {
         var remChars = this.base < 36? 1 : 2;
-        this.result = this.result.substr(0,(len-remChars));
-        this.decResult = this.convert(this.result);
+        this.result.base = this.result.base.substr(0,(len-remChars));
+        this.result.decVal = this.convert(this.result.base);
       } else {
         this.zeroResult();
       }
@@ -616,6 +720,7 @@ var app = new Vue({
     deleteResult: function(index) {
       if (this.resultSets.length > index) {
         this.resultSets.splice(index,1);
+        storeItem('results',this.resultSets)
       }
     },
     toggleAllResults: function() {
@@ -633,14 +738,18 @@ var app = new Vue({
         showDec: this.showDec,
         base: this.base,
         system: this.system,
-        baseId: this.baseId
-      }
+        baseId: this.baseId,
+        showRational: this.showRational
+      };
       storeItem('options', opts);
     },
     toggleOptions: function() {
       this.showOptions = !this.showOptions;
       this.optionsHideable = !this.showOptions;
     },
+    toggleFractions: _.debounce(function() {
+      this.showRational = !this.showRational;
+    }),
     showOptPane: function() {
       if (!this.showOptions) {
         this.showOptions = true;
@@ -658,13 +767,17 @@ var app = new Vue({
       if (typeof numStr == 'string') {
         if (numStr.length>0) {
           var cs = this.truncate(numStr.trim()).split(''),
-            n=cs.length,i=0,isSup=false,cls=[],skip=false,ch;
+            n=cs.length,i=0,isSup=false,cls=[],skip=false,isFrac=false,ch;
           for (;i<n;i++) {
             ch = cs[i];
             if (/[a-z0-9]/i.test(ch)) {
               cls = [ch];
             } else {
               cls = 'sep';
+              isFrac = true;
+            }
+            if (ch == '0' &&  i == 0) {
+              cls += ' zero';
             }
             skip = false;
             switch (this.base) {
@@ -692,6 +805,9 @@ var app = new Vue({
                 break;
             }
             if (!skip) {
+              if (isFrac) {
+                cls += ' frac';
+              }
               chars.push({
                 classes: cls,
                 name: ch,
@@ -702,6 +818,18 @@ var app = new Vue({
         }
       }
       return chars;
+    },
+    convertBaseFrac: function(frac) {
+      if (frac instanceof RationalFraction) {
+        return {
+          multiple: this.toBase(frac.multiple),
+          divisor: this.toBase(frac.divisor)
+        };
+      }
+      return {
+        multiple: 0,
+        divisor: 0
+      };
     }
   }
 });
